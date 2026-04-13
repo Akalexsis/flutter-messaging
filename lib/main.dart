2b3ec13708a5a0627ff0dd8dda076793ce0ad0c3
Kayla Thornton <kaylathornton@Kaylas-MacBook-Air.local>
2026-04-13 19:18:41 -0400

    initialize - create new message object

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'firebase_options.dart';
+import './service/messagingService.dart';
 
 @pragma('vm:entry-point')
 Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
@@ -23,44 +24,59 @@ Future<void> main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.yellow),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  
+  // initialze messaging service
+  final FCMService _messageService = FCMService();
+
+  String statusText = 'Waiting for a cloud message';
+  String imagePath = 'assets/images/default.jpg';
+
+  @override
+  void initState() {
+    super.initState();
+
+    // get initial message from firebase
+    _messageService.initialize(onData: (message) {
+      setState(() {
+        statusText = message.notification?.title ?? 'Payload received';
+        // imagePath = 'assets/images/${message.data['asset'] ?? 'default'}.png';
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text("Push Notifications", style: TextStyle( fontSize: 24 )),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text( "Home" ),
+            Text( statusText, style: TextStyle( fontSize: 18 ) ),
           ]
         ),
       ),
--- lib/service/messagingService.dart
+++ lib/service/messagingService.dart
@@ -1,3 +1,8 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import '../firebase_options.dart';
+
 class FCMService {
     // create new messaging instance
     final FirebaseMessaging messaging = FirebaseMessaging.instance;
